23fb057512855cac65f210bdf9578885c6c842f6
Nicholas Peters <npnickster@gmail.com>
2017-09-23 16:54:14 -0400

    Added jsoncpp for config file loading

--- ucf_sub/src/sub_apps/sub_thruster_manager/src/sub_thruster_manager_node.cpp
+++ ucf_sub/src/sub_apps/sub_thruster_manager/src/sub_thruster_manager_node.cpp
@@ -1,11 +1,18 @@
 #include "sub_thruster_library/t200_thruster.h"
-#include "sub_thruster_library/seabotix_thruster.h"
+//#include "sub_thruster_library/seabotix_thruster.h"
 #include "sub_thruster_library/generic_thruster.h"
 
+#include <json/json.h>
+
 #include <ros/ros.h>
-#include <geometry_msgs/Twist.h>
 #include <diagnostic_msgs/DiagnosticStatus.h>
 #include <diagnostic_msgs/SelfTest.h>
+
+#include <iostream>
+#include <fstream>
+
+//TODO: Determine module name at runtime (needs some capemgr fuckery)
+#define SUB_SECTION_NAME "COMPUTE"
 
 inline const std::string BoolToString(const bool b); //http://stackoverflow.com/a/29798
 
@@ -14,6 +21,7 @@ class ThrusterManager {
     ros::Subscriber command_subscriber;
     ros::Publisher diagnostics_output;
 
+    sub_trajectory::ThrusterCmd savedMsg;
     ros::AsyncSpinner spinner;
     
     std::map<int, GenericThruster> thrusterMap;
@@ -21,12 +29,16 @@ class ThrusterManager {
 public:
     ThrusterManager() : spinner(1)
     {
+        for(int i = 0; i < 8; i++) {
+            savedMsg.cmd.push_back(0.0);
+        }
+
         command_subscriber = nh_.subscribe("/thrusters/cmd_vel", 1000, &ThrusterManager::thrusterCb, this);
 
         diagnostics_output = nh_.advertise<diagnostic_msgs::DiagnosticStatus>("/diagnostics", 1000);
     	
-    	thrusterMap[0] = T200Thruster(1, 0x2D);
-    	thrusterMap[1] = T200Thruster(1, 0x2E); //This should keep a reference? The copy disabling thing should keep us safe
+    	//thrusterMap[0] = T200Thruster(1, 0x2D);
+    	//thrusterMap[1] = T200Thruster(1, 0x2E); //This should keep a reference? The copy disabling thing should keep us safe
     }
 
     void init()
@@ -34,9 +46,26 @@ public:
         if(spinner.canStart())
             spinner.start();
         else
+            return;   
+        
+        ifstream configFile("config.json");
+        if(!ifstream.is_open()){
+            ROS_ERROR("%s thruster controller couldn't open config file", SUB_SECTION_NAME);
             return;
-            
-    	//TODO: Iterate through the thruster param and build thruster objects.
+        }
+        
+        Json::Reader reader;
+        Json::Value obj;
+        reader.parse(configFile, obj);
+
+        Json::Value& thrustersJson = obj[SUB_SECTION_NAME];
+        for(int i = 0; i < thrustersJson.size(); i++) {
+            int thrusterID = thrustersJson[i]["ID"].asInt();
+            int thrusterType = thrustersJson[i]["Type"].asInt();
+            int thrusterAddress = thrustersJson[i]["Address"].asInt();
+            thrusterMap[thrusterID] = T200Thruster(1, thrusterAddress);
+        }
+        //TODO: Iterate through the thruster param and build thruster objects.
     	//No fucking idea how you build things from JSON in C++ but ROS does it somehow.
 
         ros::Rate rate(4);
@@ -46,20 +75,21 @@ public:
             status.name = "Thrusters";
             status.hardware_id = "Thrusters";
             
-			for(std::map<int, GenericThruster>::iterator iter = thrusterMap.begin(); iter != thrusterMap.end(); iter++)
+			for(auto& thruster : thrusterMap)
 			{
-				iter->second.updateStatus();
+				iter.second.updateStatus();
 				
-				if (thrusterOk(iter->second) && status.level != status.ERROR)
+				if (thrusterOk(iter.second) && status.level != status.ERROR)
 					status.level = status.OK;
 				else
 					status.level = status.ERROR;
 				
-				PushDiagData(status, iter->second, std::to_string(iter->first));
+				PushDiagData(status, iter.second, std::to_string(iter.first));
 				
-				iter->second.setVelocity(msg.at(iter->first));
-			}
-			
+				iter.second.setVelocity(savedMsg.cmd.at(iter.first));
+            }
+            
+            diagnostics_output.publish(status);
             rate.sleep();
         }
         spinner.stop();
@@ -97,7 +127,7 @@ public:
 
     void thrusterCb(const sub_trajectory::ThrusterCmd &msg)
     {
-		
+		savedMsg = msg;
     }
     
     float magnitude(float x, float y) //return the magnitude of a 2d vector
