ebf66f7eb59f5c049a808e23231e48e2598657c6
Nicholas Peters <npnickster@gmail.com>
2017-10-10 21:44:30 -0400

    Improved error checking/self test, added ros service to reinit thrusters

--- ucf_sub/src/sub_apps/sub_thruster_manager/src/sub_thruster_manager_node.cpp
+++ ucf_sub/src/sub_apps/sub_thruster_manager/src/sub_thruster_manager_node.cpp
@@ -5,6 +5,7 @@
 #include <json/json.h>
 
 #include <ros/ros.h>
+#include <std_msgs/Empty.h>
 #include <diagnostic_msgs/DiagnosticStatus.h>
 #include <diagnostic_msgs/SelfTest.h> //TODO: implement self tests
 
@@ -20,15 +21,16 @@ class ThrusterManager {
     ros::NodeHandle nh_;
     ros::Subscriber command_subscriber;
     ros::Publisher diagnostics_output;
+    self_test::TestRunner self_test_;
 
     sub_trajectory::ThrusterCmd savedMsg;
     
     std::map<int, GenericThruster> thrusterMap;
 
-    
+    ros::ServiceServer initServer;
 
 public:
-    ThrusterManager() : spinner(1)
+    ThrusterManager() : spinner(1), self_test_()
     {
         for(int i = 0; i < 8; i++) {
             savedMsg.cmd.push_back(0.0);
@@ -37,24 +39,38 @@ public:
         command_subscriber = nh_.subscribe("/thrusters/cmd_vel", 1000, &ThrusterManager::thrusterCb, this);
 
         diagnostics_output = nh_.advertise<diagnostic_msgs::DiagnosticStatus>("/diagnostics", 1000);
+
+        self_test_.add("Test connections", this, &ThrusterManager::testThrusterConnections);
+
+        initServer = nh_.advertiseService("initThrusters", &ThrusterManager::initService, this);
     	
     	//thrusterMap[0] = T200Thruster(1, 0x2D);
     	//thrusterMap[1] = T200Thruster(1, 0x2E); //This should keep a reference? The copy disabling thing should keep us safe
     }
 
-    void init()
-    {     
-        ifstream configFile("config.json");
+    Json::Value loadConfig(std::string filename)
+    {
+        ifstream configFile(filename);
         if(!ifstream.is_open()){
             ROS_ERROR("%s thruster controller couldn't open config file", SUB_SECTION_NAME);
+            status.summary(diagnostic_msgs::DiagnosticStatus::ERROR, "Config file didn't load");
             return;
         }
 
         Json::Reader reader;
         Json::Value obj;
         reader.parse(configFile, obj);
+        return obj;
+    }
+    void initService(std_msgs::Empty& req, std_msgs::Empty& resp)
+    {
+        init();
+    }
 
-        Json::Value& thrustersJson = obj[SUB_SECTION_NAME];
+    void init()
+    {     
+        thrusterMap.clear();
+        Json::Value& thrustersJson = loadConfig("config.json")[SUB_SECTION_NAME];
         for(int i = 0; i < thrustersJson.size(); i++) {
             int thrusterID = thrustersJson[i]["ID"].asInt();
             int thrusterType = thrustersJson[i]["Type"].asInt(); //TODO: support for multiple thruster types
@@ -80,7 +96,7 @@ public:
 
     void spin()
     {
-        ros::Rate rate(4);
+        ros::Rate rate(10);
         while(ros::ok()) {
             //Publish diagnostic data here
             diagnostic_msgs::DiagnosticStatus status;
@@ -94,12 +110,7 @@ public:
                     iter.second.setVelocity(savedMsg.cmd.at(iter.first));
                 } catch(I2CException e) {
                     //Publish an error message for the diagnostic system to do something about
-                    diagnostic_msgs::DiagnosticStatus status;
-                    status.name = "Thrusters";
-                    status.hardware_id = "Thrusters";
                     status.level = status.ERROR;
-                    diagnostics_output.publish(status);
-                    ros::spinOnce();
                 }
 				
 				if (thrusterOk(iter.second) && status.level != status.ERROR)
@@ -112,6 +123,7 @@ public:
             
             diagnostics_output.publish(status);
             ros::spinOnce();
+            self_test_.checkTest();
             rate.sleep();
         }
     }
@@ -154,29 +166,27 @@ public:
     //Self test function
     void testThrusterConnections(diagnostic_updater::DiagnosticStatusWrapper& status)
     {
-        ifstream configFile("config.json");
-        if(!ifstream.is_open()){
-            ROS_ERROR("%s thruster controller couldn't open config file", SUB_SECTION_NAME);
-            status.summary(diagnostic_msgs::DiagnosticStatus::ERROR, "Config file didn't load");
-            return;
-        }
-
-        Json::Reader reader;
-        Json::Value obj;
-        reader.parse(configFile, obj);
-
-        Json::Value& thrustersJson = obj[SUB_SECTION_NAME];
+        self_test_.setID("thrusterController");
+        std::stringstream failedThrusters;
+        Json::Value& thrustersJson = loadConfig("config.json")[SUB_SECTION_NAME];
         for(int i = 0; i < thrustersJson.size(); i++) {
             int thrusterID = thrustersJson[i]["ID"].asInt();
             int thrusterType = thrustersJson[i]["Type"].asInt(); //TODO: support for multiple thruster types
             int thrusterAddress = thrustersJson[i]["Address"].asInt();
+            
             try{
-                T200Thruster(1, thrusterAddress);
+                T200Thruster(1, thrusterAddress).updateStatus();
             }
             catch(I2CException e){
-                status.summary(diagnostic_msgs::DiagnosticStatus::ERROR, "Thruster couldn't connect");
-                status.add("Fail id", thrusterAddress); //TODO: What if multiple thrusters fail
+                failedThrusters << thrusterAddress << " ";
             }
+        }
+
+        if(failedThrusters.str().length() > 0){
+            status.add("Failures", failedThrusters.str());
+            status.summary(diagnostic_msgs::DiagnosticStatus::ERROR, "Thruster couldn't connect");
+        } else {
+            status.summary(diagnostic_msgs::DiagnosticStatus::OK, "All thrusters OK");
         }
     }
 };
