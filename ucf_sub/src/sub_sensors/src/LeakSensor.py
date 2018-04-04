6a6a1e9eb6fd88f95b0ce834202689732db5cd67
Azimath <npnickster@gmail.com>
2018-04-04 01:06:37 +0000

    tested and corrected Leak Sensor

--- ucf_sub/src/sub_sensors/src/LeakSensor.py
+++ ucf_sub/src/sub_sensors/src/LeakSensor.py
@@ -1,14 +1,16 @@
+#!/usr/bin/env python
 import RPi.GPIO as GPIO
 import rospy
-from std_msgs import Bool
-from diagnostic_msgs import DiagnosticArray
+from std_msgs.msg import Bool
+from diagnostic_msgs.msg import DiagnosticArray, DiagnosticStatus
 
-CHANNEL =
+CHANNEL = 17
 
 def publish():
 	leakPub = rospy.Publisher('Leak', Bool, queue_size=1)
-	diagPub = rospy.Publisher('/diagnostic', DiagnosticArray)
+	diagPub = rospy.Publisher('/diagnostic', DiagnosticArray, queue_size=1)
 	rospy.init_node('Leak')
+	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(CHANNEL, GPIO.IN)
 	freq = rospy.Rate(1)
 	leak = Bool()
@@ -17,8 +19,8 @@ def publish():
 	while not rospy.is_shutdown():
 		levelData=0
 		if (leak.data):
-			diag.status[0].level=2
-		diag.status = [DiagnosticStatus(name='Leak', message=str(leak.data), level=levelData]
+			levelData=2
+		diag.status = [DiagnosticStatus(name='Leak', message=str(leak.data), level=levelData)]
 		leak.data = GPIO.input(CHANNEL)
 		leakPub.publish(leak)
 		diagPub.publish(diag)
