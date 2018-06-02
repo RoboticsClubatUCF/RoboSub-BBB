a6e374858d531cc6b79c22ca12eccc66b6b1a28b
Nicholas Peters <npnickster@gmail.com>
2018-06-02 16:31:35 -0400

    Update MOBUSDepth.py
    Fixed struct.unpack returning list, made temperature sample less often

--- ucf_sub/src/sub_sensors/src/MOBUSDepth.py
+++ ucf_sub/src/sub_sensors/src/MOBUSDepth.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python
-
-
-#!/usr/bin/env python
 import rospy
 import struct
 from pymodbus.client.sync import ModbusSerialClient as modbus
@@ -16,12 +13,17 @@ def publish():
 	rospy.init_node('Depth')
 	temp = Temperature()
 	depth = Float32()
-	freq = rospy.Rate(1)
+	freq = rospy.Rate(60)
+	loop = 0
 	while not rospy.is_shutdown():
-		rr = sensor.read_holding_registers(address=8, count=2, unit=1)
-		temp.temperature = struct.unpack('>f',struct.pack('>HH', *rr.registers)) 
+		if loop >= 30:
+			rr = sensor.read_holding_registers(address=8, count=2, unit=1)
+			temp.temperature = struct.unpack('>f',struct.pack('>HH', *rr.registers))[0]
+			loop = 0
+		loop += 1
+			
 		rr = sensor.read_holding_registers(address=2, count=2, unit=1)
-		depth.data = struct.unpack('>f',struct.pack('>HH', *rr.registers))
+		depth.data = struct.unpack('>f',struct.pack('>HH', *rr.registers))[0]
 		tempPub.publish(temp)
 		depthPub.publish(depth)
 		freq.sleep()
