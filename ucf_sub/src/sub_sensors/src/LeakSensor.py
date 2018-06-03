d8a6ffaf99799db1946d725b38f741b98a3c4e30
darien-craig <darien@craigs.us>
2018-06-02 22:46:24 -0400

    Updated leak sensor for jetson

--- ucf_sub/src/sub_sensors/src/LeakSensor.py
+++ ucf_sub/src/sub_sensors/src/LeakSensor.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
+import gpio
 import rospy
 from std_msgs.msg import Bool
 from diagnostic_msgs.msg import DiagnosticArray, DiagnosticStatus
 
-CHANNEL = 17
+CHANNEL = 429 #should be 17
 
 def publish():
 	leakPub = rospy.Publisher('Leak', Bool, queue_size=1)
 	diagPub = rospy.Publisher('/diagnostic', DiagnosticArray, queue_size=1)
 	rospy.init_node('Leak')
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setup(CHANNEL, GPIO.IN)
+	#GPIO.setmode(GPIO.BCM)
+	#GPIO.setup(CHANNEL, GPIO.IN)
+	GPIO.setup(CHANNEL, gpio.IN)
 	freq = rospy.Rate(1)
 	leak = Bool()
 	pub = rospy.Publisher('/diagnostics', DiagnosticArray, queue_size=1)
@@ -20,8 +22,8 @@ def publish():
 		levelData=0
 		if (leak.data):
 			levelData=2
-		diag.status = [DiagnosticStatus(name='Leak', message=str(leak.data), level=levelData)]
 		leak.data = GPIO.input(CHANNEL)
+		diag.status = [DiagnosticStatus(name='Leak', message=str(leak.data), level=levelData)]
 		leakPub.publish(leak)
 		diagPub.publish(diag)
 		freq.sleep()
