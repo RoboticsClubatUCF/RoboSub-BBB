7e33f3a2b340d3978c1b39441cd62698172798e3
Otaku-AI <rfatt13@gmail.com>
2018-06-24 19:11:00 -0400

    Fixed InternalEnvironment errors

--- ucf_sub/src/sub_sensors/src/InternalEnvironment.py
+++ ucf_sub/src/sub_sensors/src/InternalEnvironment.py
@@ -1,21 +1,22 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import HIH6130
 import rospy
-from sensor_msgs import Temperature
-from sensor_msgs import RelativeHumidity
+from sensor_msgs.msg import Temperature, RelativeHumidity
 
 def publish():
 	tempPub = rospy.Publisher('InternalTemperature', Temperature, queue_size=1)
 	humidPub = rospy.Publisher('InternalHumidity', RelativeHumidity, queue_size=1)
 	rospy.init_node('InternalEnvironment')
-	sensor = HIH3160()
+	sensor = HIH6130.HIH6130(bus=1)
 	temp = Temperature()
+	temp.header.frame_id = "base_link"
 	humid = RelativeHumidity()
-	freq = rospy.Rate(1)
+	humid.header.frame_id = "base_link"
+	freq = rospy.Rate(5)
 	while not rospy.is_shutdown():
 		sensor.read()
 		temp.temperature = sensor.t
-		humid.relative_humidity = sensor.h
+		humid.relative_humidity = sensor.rh
 		tempPub.publish(temp)
 		humidPub.publish(humid)
 		freq.sleep()
